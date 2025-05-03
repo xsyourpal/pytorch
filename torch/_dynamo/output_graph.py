36609198024c1cedcf8a131c27ef8e0772e6c478
bobrenjc93 <bobren@meta.com>
2025-05-02 17:51:47 -0700

    add codegen layer specialization dispatch
    ghstack-source-id: ec521e2e435f1308f135ccb9b12aeab1061772da
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/152670

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -1496,7 +1496,7 @@ class OutputGraph(OutputGraphGuardsState):
                 # a lot of fake_tensor ownership assumptions and runs afoul of detect_fake_mode
                 self.tracing_context.fake_mode = backend_fake_mode
 
-            compiled_fns = []
+            specialized_compiled_fns = []
             with self.restore_global_state():
                 compiled_fn = self.call_user_compiler(gm)
                 for specialization in old_fake_mode.shape_env.backend_specializations:
@@ -1538,7 +1538,13 @@ class OutputGraph(OutputGraphGuardsState):
             self.install_global_unsafe(name, compiled_fn)
 
             cg = PyCodegen(tx)
-            cg.make_call_generated_code(name)
+
+            if specialized_compiled_fns:
+                for name, specialized_compiled_fn in specialized_compiled_fns:
+                    self.install_global_unsafe(name, specialized_compiled_fn)
+                cg.make_call_specialized_code(name, specialized_compiled_fns)
+            else:
+                cg.make_call_generated_code(name)
             return cg.get_instructions()
 
     @property
