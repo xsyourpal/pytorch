8b0012ef7ff96440a86401b755f6557c7afd4de1
Will Feng <yfeng.us@gmail.com>
2025-05-06 15:08:47 -0700

    [WIP] fix issue 151198
    ghstack-source-id: 772b4dfce6d9ce4b70730afe053ba07b3e7d45df
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/152764

---
+++ test/test_repro_151198_2d.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch._inductor import config
+import os
+
+config.fallback_random = True
+torch.set_grad_enabled(False)
+torch.manual_seed(0)
+
+
+class Model(torch.nn.Module):
+
+    def __init__(self):
+        super(Model, self).__init__()
+        self.pad = torch.nn.ReflectionPad2d(1)  # this fails
+        # self.pad = torch.nn.ReplicationPad2d(1)  # this also fails
+        # self.pad = torch.nn.CircularPad2d(1)  # this works
+        # self.pad = torch.nn.ZeroPad2d(1)  # this works
+
+    def forward(self, x):
+        x = self.pad(x)
+        x = x.view(x.size(0), -1)
+        x = torch.chunk(x, 2, dim=1)
+        return torch.sum(x[0] - x[1])
+
+
+model = Model().eval().cuda()
+
+
+x = torch.randn(2, 3, 4, 4).cuda()
+
+inputs = [x]
+
+
+def run_test(model, inputs, backend):
+    if backend != "eager":
+        model = torch.compile(model, backend=backend)
+    torch.manual_seed(0)
+    output = model(*inputs)
+    return output
+
+
+output = run_test(model, inputs, 'eager')
+c_output = run_test(model, inputs, 'inductor')
+fp64 = run_test(model.to(dtype=torch.float64), [x.to(dtype=torch.float64)], 'eager')
+
+
+print(output)
+print(c_output)
+print(fp64)
+print(torch.allclose(output, c_output, 1e-3, 1e-3, equal_nan=True))
+print(torch._dynamo.utils.same(output, c_output, fp64))
+print(torch.max(torch.abs(output - c_output)))
